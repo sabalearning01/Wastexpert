bb5a45a23693d52b9fcee033c48b50fcf960bb7f
sabalearning01 <sabatemitayo44@gmail.com>
2024-07-17 12:08:47 +0100

    services completed

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -6,6 +6,11 @@ import underline from '../../Images/underline.png'
 import play from '../../Images/play.png'
 import IMG2 from '../../Images/IMG2.png'
 import GCRRecycling from '../../Images/GCRRecycling.png'
+import newbg from '../../Images/newbg.png'
+import Footer from '../../Components/Footer/Footer'
+import  waste from '../../Images/waste.png'
+import flip from '../../Images/flip.png'
+
 
  const Services = () => {
   return (
@@ -43,10 +48,22 @@ import GCRRecycling from '../../Images/GCRRecycling.png'
       </div>
 
 
+      <img  className="new" src={newbg} alt="wastebackground"/>
+      
+      <div className='Other'>
+         <h1>Other Services</h1> 
+         <img src ={underline} alt="line"/>
 
-      <div className='bgimg1'>
-      <img  className="bgimg1" src={GCRRecycling} alt="GCRecyclingimage" />  
-         <h1>Other Services</h1>
+         <div className='boxcontainers'>
+      <div className='containers'><img src={waste} alt="wasteimage"/><h4>Corporate</h4>  <p>Nisl, dictumst eu urna, at odio scelerisque pulvinar a, magna. Vulputate. sit amet, consectetur adipiscing elit. Mollis leo auctor a scelerisque vel sit cras eu consectetur. Viverra venenatis, mauris diam dolor cras a. Lectus massa aenean neque pulvinar tortor, a. Nisl, dictumst eu urna, at odio scelerisque pulvinar a, magna. Vulputate. consectetur adipiscing elit. Mollis leo auctor a</p><button className='getcta'>Get Started</button> </div>
+      <div className='containers'><img src={flip} alt="flipimage"/><h4>Flip it</h4><p>Nisl, dictumst eu urna, at odio scelerisque pulvinar a, magna. Vulputate. sit amet, consectetur adipiscing elit. Mollis leo auctor a scelerisque vel sit cras eu consectetur. Viverra venenatis, mauris diam dolor cras a. Lectus massa aenean neque pulvinar tortor, a. Nisl, dictumst eu urna, at odio scelerisque pulvinar a, magna. Vulputate. consectetur adipiscing elit. Mollis leo auctor a</p><button className='getcta'>Get Started</button></div>
+      <div className='containers'> <img src={waste} alt="wasteimage"/> <h4>Recce</h4><p>Nisl, dictumst eu urna, at odio scelerisque pulvinar a, magna. Vulputate. sit amet, consectetur adipiscing elit. Mollis leo auctor a scelerisque vel sit cras eu consectetur. Viverra venenatis, mauris diam dolor cras a. Lectus massa aenean neque pulvinar tortor, a. Nisl, dictumst eu urna, at odio scelerisque pulvinar a, magna. Vulputate. consectetur adipiscing elit. Mollis leo auctor a</p><button className='getcta'>Get Started</button> </div>
+      
+     </div>
+
+ <Footer/> 
+
+
       </div>
      
 
